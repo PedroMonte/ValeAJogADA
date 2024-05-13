bc590bbd456b8074b4cdfc715756807ccd609eb4
PedroMonte <pedrovitor23.monte@gmail.com>
2024-05-13 17:25:41 -0300

    create base project

---
+++ ValeAJogADA/ValeAJogADA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ValeAJogADA
+//
+//  Created by Pedro Vitor de Oliveira Monte on 13/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ValeAJogADA/ValeAJogADA/ValeAJogADAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValeAJogADAApp.swift
+//  ValeAJogADA
+//
+//  Created by Pedro Vitor de Oliveira Monte on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ValeAJogADAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
